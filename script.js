6286a39940d834ea400dd045db077226e5591b9a
Lakshya Birla <Lakshyabirla7cr@gmail.com>
2023-08-16 16:10:56 -0600

    Changes #2

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function toggleMode() {
 
   if (iconBars.classList.contains('fa-bars')) {
     iconBars.classList.remove('fa-bars');
-    iconBars.classList.toggle('fa-x');
+    iconBars.classList.add('fa-x');
     iconBars.style.transform = 'rotate(360deg)';
   } else if (iconBars.classList.contains('fa-x')) {
     iconBars.classList.remove('fa-x');
-    iconBars.classList.toggle('fa-bars');
+    iconBars.classList.add('fa-bars');
     iconBars.style.transform = 'rotate(180deg)';
   }
 }
@@ -22,17 +22,42 @@ function mobileMenu() {
   navItems.classList.toggle('active');
 }
 
+const navLinks = document.querySelectorAll('.nav-item a');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', smoothScroll);
+});
+
+function smoothScroll(e) {
+  e.preventDefault();
+  const targetId = this.getAttribute('href');
+  const targetElement = document.querySelector(targetId);
+
+  if (targetElement) {
+    const offset = targetElement.offsetTop - 70; // Adjust the offset as needed
+    window.scrollTo({
+      top: offset,
+      behavior: 'smooth',
+    });
+
+    // Close the mobile menu on link click
+    toggleMode();
+    mobileMenu();
+  }
+}
+
+// Function for sticky navbar
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('nav');
+  navbar.classList.toggle('sticky', window.scrollY > 2);
+});
+
+// Function to close mobile menu on link click
+function closeMenu() {
+  hamburger.classList.remove('active');
+  navItems.classList.remove('active');
+}
+
 const navLink = document.querySelectorAll('.nav-link');
 
 navLink.forEach((n) => n.addEventListener('click', closeMenu));
-
-function closeMenu() {
-  hamburger.classList.remove('active');
-  navMenu.classList.remove('active');
-}
-
-//fucntion for stikcy navbar
-window.addEventListener('scroll', function () {
-  var navbar = document.querySelector('nav');
-  navbar.classList.toggle('sticky', window.scrollY > 2);
-});
